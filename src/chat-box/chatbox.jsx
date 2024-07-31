268d7e3146d2eba94341a3f8915cd56b83e9d51f
kishan <bourntecrepo@gmail.com>
2024-07-31 11:59:22 +0530

    new-changes

--- src/chat-box/chatbox.jsx
+++ src/chat-box/chatbox.jsx
@@ -6,122 +6,148 @@ import { FaUser } from "react-icons/fa";
 import chatbutton from "./images/chat.svg";
 import chatlogo from "./images/chat-logo.svg";
 import sendchat from "./images/send-chat.svg";
-import { DotLoader } from "react-spinners";
+import { BeatLoader } from "react-spinners";
 import ReactPlayer from "react-player";
-
 // YouTubeEmbed component memoized
-const YouTubeEmbed = memo(({ url }) => (
-  <div>
-    <div style={{ marginTop: "20px" }}>
-      <ReactPlayer url={url} controls={true} width="230px" height="170px" />
+const YouTubeEmbed = memo(
+  ({ url }) => (
+    <div>
+      <div style={{ marginTop: "20px" }}>
+        <ReactPlayer url={url} controls={true} width="230px" height="170px" />
+      </div>
+      <a
+        href={url}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="youtube-link"
+      >
+        {url}
+      </a>
     </div>
-    <a href={url} target="_blank" rel="noopener noreferrer" className="youtube-link">
-      {url}
-    </a>
-  </div>
-), (prevProps, nextProps) => prevProps.url === nextProps.url);
+  ),
+  (prevProps, nextProps) => prevProps.url === nextProps.url
+);
+
+// ChatMessage components memoized
+const BASE_URL = 'http://103.127.143.141:3000/';
+const ChatBotMessage = ({ messageObj }) => {
+  const lines = convertMessage(messageObj.message);
+  return <div>{lines}</div>;
+};
+const convertMessage = (message) => {
+  return message.split('<br>').map((line, index) => (
+    <React.Fragment key={index}>
+      {line}
+      {index < message.split('<br>').length - 1 && <br />}
+    </React.Fragment>
+  ));
+};
 
 
-
-// ChatMessage components memoized   
-const ChatMessage = memo(({ msg }) => (
-  <div className="chat-box-body" key={msg.id}>
-    {msg.messageType !== "out" && msg.messageType !== "greet" && (
-      <div className="chat-box-right-send">
-      <div className="chat-box-body-send">
-        <p>{msg.message}</p>
-      </div>
-      <div className="chat-box-time">Today {msg.time}</div>
-
-      </div>
-    )}
-      {/* {msg.messageType !== "out" && msg.messageType !== "greet" && (
-      <div className="chat-box-time">Today {msg.time}</div>
-    )}
-      */}
-    {msg.messageType === "greet" && (
-      <div className="chat-box-left">
-      <div className="chat-box-body-greet">
-      {/* <img src={chatlogo} className="chat-icon-from-top" alt="Chat Icon" /> */}
-
-        {/* <FaUser /> */}
-        <p>{msg.message}</p>
-      </div>
-      <div className="chat-box-time">Today {msg.time}</div>
-
-      </div>
-    )}
-     {/* {msg.messageType === "greet" && (
-      <div className="chat-box-timer">Today {msg.time}</div>
-    )} */}
- 
-    {msg.messageType === "out" && (
-          <div className="chat-box-left">
-
-      <div className="chat-box-body-receive">
-        <img src={chatlogo} className="chat-icon-from-top" alt="Chat Icon" />
-        <p className="chat-txt-hed">HMIS</p>
-        <p className="chat-txt-cont">{msg.message}</p>
-        {msg.source === "youtube_video" && <YouTubeEmbed url={msg.link} />
-       
-        }
-      </div>
-      </div>
-      
-    )}
-        {msg.messageType === "out" && msg.source === "youtube_video" && (
-          <div className="chat-box-left">
-      <div className="chat-box-fotr-bottom">
-        <div className="chat-txt-hed">
-          Source: <span className="clr-black">{msg.source}</span>
-       
-
+const ChatMessage = memo(
+  ({ msg, isLoading, isLatestMessage }) => (
+    <div className="chat-box-body" key={msg.id}>
+      {msg.messageType !== "out" && msg.messageType !== "greet" && (
+        <div className="chat-box-right-send">
+          <div className="chat-box-body-send">
+            <p>{msg.message}</p>
+          </div>
+          <div className="chat-box-time">Today {msg.time}</div>
         </div>
+      )}
 
-      </div>
-      <div className="chat-box-time">Today {msg.time}</div>
+      {msg.messageType === "greet" && (
+        <div className="chat-box-left"  >
+          <div className="chat-box-body-greet">
+            {isLatestMessage && (
+              <img
+                src={chatlogo}
+                className="chat-icon-from-top"
+                alt="Chat Icon"
+              />
+            )}
 
-      </div>
-    )}
+<p>{msg.message}</p>
 
-    {msg.messageType === "out" && msg.source === "user_manual" && (
-          <div className="chat-box-left">
-
-      <div className="chat-box-fotr-bottom">
-        <div className="chat-txt-hed">
-          Source: <span className="clr-black">{msg.source}</span>
-          <a
-            href={`${process.env.PUBLIC_URL}/${msg.link}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{ marginLeft: '10px' }}
-          >
-              Document
-          </a>
+          </div>
+          <div className="chat-box-time">Today {msg.time}</div>
         </div>
-      </div>
-      <div className="chat-box-time">Today {msg.time}</div>
+      )}
 
-      </div>
-    )}
+      {msg.messageType === "out" && (
+        <div className="chat-box-left">
+          <div className="chat-box-body-receive">
+            {isLatestMessage && (
+              <img
+                src={chatlogo}
+                className="chat-icon-from-top-new {
+"
+                alt="Chat Icon"
+              />
+            )}
 
-{msg.messageType === "out" && msg.source === "QnA" && (
-          <div className="chat-box-left">
-      <div className="chat-box-fotr-bottom">
-        <div className="chat-txt-hed">
-          Source: <span className="clr-black">{msg.source}</span>
+            <p className="chat-txt-hed">HMIS</p>
+          <p  className="chat-txt-cont"><ChatBotMessage messageObj={msg} /></p>  
+            <div className="youtube-url">
+              {msg.source === "youtube_video" && (
+                <YouTubeEmbed url={msg.link} />
+              )}
+            </div>
+          </div>
         </div>
+      )}
+      {msg.messageType === "out" && msg.source === "youtube_video" && (
+        <div className="chat-box-left">
+          <div className="chat-box-fotr-bottom">
+            <div className="chat-txt-hed">
+              Source: <span className="clr-black">Youtube Video</span>
+            </div>
+          </div>
+          <div className="chat-box-time">Today {msg.time}</div>
+        </div>
+      )}
+
+      {msg.messageType === "out" && msg.source === "user_manual" && (
+        <div className="chat-box-left">
+          <div className="chat-box-fotr-bottom">
+            <div className="chat-txt-hed">
+              Source: <span className="clr-black">User Manual</span>
+              <a
+                href={`${BASE_URL}/${msg.doc_url}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ marginLeft: "10px" }}
+              >
+                Document
+              </a>
+            </div>
+          </div>
+          <div className="chat-box-time">Today {msg.time}</div>
+        </div>
+      )}
+
+      {msg.messageType === "out" && msg.source === "QnA" && (
+        <div className="chat-box-left">
+          <div className="chat-box-fotr-bottom">
+            <div className="chat-txt-hed">
+              Source: <span className="clr-black">{msg.source}</span>
+            </div>
+          </div>
+          <div className="chat-box-time">Today {msg.time}</div>
+        </div>
+      )}
+
+      <div className="loading-spinner loading">
+        <BeatLoader color="#C5EBF9" size={12} loading={isLoading} />
       </div>
-      <div className="chat-box-time">Today {msg.time}</div>
-      </div>
-    )}
-   
-  </div>
-), (prevProps, nextProps) => prevProps.msg.id === nextProps.msg.id);
+    </div>
+  ),
+  (prevProps, nextProps) =>
+    prevProps.msg.id === nextProps.msg.id &&
+    prevProps.isLoading === nextProps.isLoading
+);
 
-
-
-// Chat Box  Main Component 
+// Chat Box  Main Component
 const ChatBox = () => {
   const [chatBoxVisible, setChatBoxVisible] = useState(false);
   const [modalVisible, setModalVisible] = useState(false);
@@ -130,6 +156,7 @@ const ChatBox = () => {
   const [messages, setMessages] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [showTooltip, setShowTooltip] = useState(false);
+  const [latestMessageId, setLatestMessageId] = useState(null);
 
   const handleMouseEnter = () => setShowTooltip(true);
   const handleMouseLeave = () => setShowTooltip(false);
@@ -137,6 +164,8 @@ const ChatBox = () => {
   const hideLoading = () => setIsLoading(false);
 
   axios.defaults.baseURL = "http://127.0.0.1:8000";
+
+  console.log(isLoading);
 
   const handleChatButtonClick = () => setChatBoxVisible(true);
   const handleCloseButtonClick = () => {
@@ -146,11 +175,13 @@ const ChatBox = () => {
     setInputValue("");
   };
 
+  console.log(messages);
   const handleModalToggle = () => setModalVisible(!modalVisible);
   const handleInputChange = (e) => setInputValue(e.target.value);
 
   useEffect(() => {
-    const savedMessages = JSON.parse(localStorage.getItem("chatMessages")) || [];
+    const savedMessages =
+      JSON.parse(localStorage.getItem("chatMessages")) || [];
     setMessages(savedMessages);
   }, []);
 
@@ -161,10 +192,7 @@ const ChatBox = () => {
     }
   };
 
-
-
-
-  const displayMessage = useCallback((message, source, messageType, link) => {
+  const displayMessage = useCallback((message, source, messageType, link, doc_url) => {
     const now = new Date();
     const hours = now.getHours().toString().padStart(2, "0");
     const minutes = now.getMinutes().toString().padStart(2, "0");
@@ -172,6 +200,7 @@ const ChatBox = () => {
     // const formattedTime = now.toLocaleTimeString([], { hour: 'numeric', minute: '2-digit', hour12: true });
 
     const formattedMessage = message.replace(/\n/g, "<br>");
+
     // const formattedTime = time.toLocaleTimeString([], { hour: 'numeric', minute: '2-digit', hour12: true });
 
     // console.log(formattedTime);
@@ -182,38 +211,53 @@ const ChatBox = () => {
       link: link,
       id: Date.now(),
       time: `${hours}:${minutes}`,
-
+      doc_url:doc_url,
     };
 
     setMessages((prevMessages) => {
       const updatedMessages = [...prevMessages, messageObj];
       localStorage.setItem("chatMessages", JSON.stringify(updatedMessages));
+      setLatestMessageId(messageObj.id);
       return updatedMessages;
     });
   }, []);
 
-  const sendToBackend = useCallback((userInput) => {
+  const sendToBackend = useCallback(
+    (userInput) => {
+      showLoading();
+      fetch("http://103.127.143.141:3000/chatbot/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-CSRFToken": getCookie("csrftoken"),
+        },
+        body: JSON.stringify({ user_input: userInput }),
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then((data) => {
+          if (data.greet) displayMessage(data.greet, "greet", "greet");
+          if (data.QnA) displayMessage(data.QnA, "QnA", "out");
+          if (data.gpt_answer)
+            displayMessage(data.gpt_answer, "youtube_video", "out", data.link);
+          if (data.best_answer)
+            displayMessage(data.best_answer, "Suggested_answer", "out");
+          if (data.answer_manual)
+            displayMessage(data.answer_manual, "user_manual", "out", data.link, data.doc_url);
+          hideLoading();
+        })
+        .catch((error) => {
+          displayMessage('Network error: Unable to reach the server.', "greet", "greet");
+          hideLoading();
+        });
+    },
+    [displayMessage]
+  );
   
-    showLoading();
-    fetch("http://localhost:8000/chatbot/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "X-CSRFToken": getCookie("csrftoken"),
-      },
-
-      body: JSON.stringify({ user_input: userInput }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.greet) displayMessage(data.greet, "greet", "greet");
-        if (data.QnA) displayMessage(data.QnA, "QnA", "out");
-        if (data.gpt_answer) displayMessage(data.gpt_answer, "youtube_video", "out", data.link);
-        if (data.best_answer) displayMessage(data.best_answer, "Suggested_answer", "out");
-        if (data.answer_manual) displayMessage(data.answer_manual, "user_manual", "out", data.link);
-        hideLoading();
-      });
-  }, [displayMessage]);
 
   const sendMessage = useCallback(() => {
     if (inputValue.trim() !== "") {
@@ -248,6 +292,8 @@ const ChatBox = () => {
       chatContainer.scrollTop = scrollHeight - clientHeight;
     }
   };
+  
+
 
   useEffect(() => {
     scrollToBottom();
@@ -270,24 +316,29 @@ const ChatBox = () => {
           </span>
         )}
       </div>
-      <div className="chat-box" style={{ visibility: chatBoxVisible ? "visible" : "hidden" }}>
+      <div
+        className="chat-box"
+        style={{ visibility: chatBoxVisible ? "visible" : "hidden" }}
+      >
         <div className="chat-box-header">
           <img src={chatlogo} className="chat-log-w" alt="Chat Logo" />
           <div className="chat-box-header-head">
             RailTel <span className="hmis-assist">HMIS Assist</span>
           </div>
           <p>
-            <i className="fa fa-times close-btn-sty" onClick={handleCloseButtonClick}></i>
+            <i
+              className="fa fa-times close-btn-sty"
+              onClick={handleCloseButtonClick}
+            ></i>
           </p>
         </div>
         <div ref={chatContainerRef} className="chat-bot-middlebox">
-          {isLoading && (
-            <div className="loading">
-              <DotLoader color={"#123abc"} loading={isLoading} size={40} />
-            </div>
-          )}
-          {messages.map((msg) => (
-            <ChatMessage msg={msg} />
+          {messages.map((msg, index) => (
+            <ChatMessage
+              msg={msg}
+              isLoading={msg.id === latestMessageId && isLoading}
+              isLatestMessage={index === messages.length - 1}
+            />
           ))}
         </div>
         <ChatInput
@@ -295,8 +346,6 @@ const ChatBox = () => {
           handleInputChange={handleInputChange}
           handleKeyPress={handleKeyPress}
           sendMessage={sendMessage}
-          isLoading={isLoading}
-
         />
       </div>
       <div className="p60"></div>
@@ -312,34 +361,28 @@ const ChatBox = () => {
   );
 };
 
-
-// Input Container 
-const ChatInput = memo(({ inputValue, handleInputChange, handleKeyPress, sendMessage, isLoading }) => (
-  <div className="chat-box-footer">
-  <input
-    placeholder=" Type here..."
-    type="text"
-    value={inputValue}
-    onKeyDown={handleKeyPress}
-    onChange={handleInputChange}
-  />
-   {isLoading ? (
-            <div className="loading">
-
-      {/* <DotLoader color={"#123abc"} loading={isLoading} size={20} /> */}
-      </div>
-    ) : (
-      <i className={`send ${!inputValue.trim() ? 'disabled' : ''}`}>
+// Input Container
+const ChatInput = memo(
+  ({ inputValue, handleInputChange, handleKeyPress, sendMessage }) => (
+    <div className="chat-box-footer">
+      <input
+        placeholder=" Type here..."
+        type="text"
+        value={inputValue}
+        onKeyDown={handleKeyPress}
+        onChange={handleInputChange}
+      />
+      <i className={`send ${!inputValue.trim() ? "disabled" : ""}`}>
         <img
           src={sendchat}
           className="pad-25"
           alt="Send"
-          style={{ cursor: !inputValue.trim() ? 'not-allowed' : 'pointer' }}
+          style={{ cursor: !inputValue.trim() ? "not-allowed" : "pointer" }}
           onClick={sendMessage}
         />
       </i>
-    )}
-</div>
-));
+    </div>
+  )
+);
 
 export default ChatBox;
